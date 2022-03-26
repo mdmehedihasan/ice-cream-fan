1cd833268fe4369385330181081822bad37504db
mehedi hasan <mehedicsebu@gmail.com>
2022-03-26 14:25:21 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='nav'>
+            <h2>ICECREAM FANS</h2>
+            <p>TAKE YOUR FIRST BITE</p>
+        </nav>
+    );
+};
+
+export default Header;
